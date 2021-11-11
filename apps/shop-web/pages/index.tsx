141ad3b96e56abf77aaedf64d74d17488dfcb270
Daniele <daniele.tortora@tundra.com>
2021-11-11 15:16:36 +0100

    rename libs

--- apps/shop-web/pages/index.tsx
+++ apps/shop-web/pages/index.tsx
@@ -4,9 +4,12 @@ import {
   BootstrapButton,
 } from "@tundra/shared";
 
+import { Header } from "@tundra/core";
+
 export default function Home() {
   return (
     <div>
+      <Header />
       <PrimaryButton>Global SCSS</PrimaryButton>
       <SecondaryButton>SCSS Module</SecondaryButton>
       <BootstrapButton>Bootstrap Global</BootstrapButton>
