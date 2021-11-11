db83049166f807592ae06471aa0e2ce6c8a7e203
Daniele <daniele.tortora@tundra.com>
2021-11-11 14:52:30 +0100

    setup

--- apps/shop-web/next.config.js
+++ apps/shop-web/next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")([
+  "@tundra/shared",
+  "@tundra/core",
+]);
+const withImages = require("next-images");
+module.exports = withPlugins([withTM(), withImages], {
+  webpack: (config) => {
+    // custom webpack config
+    return config;
+  },
+  images: {},
+});
---
+++ apps/shop-web/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from "next/image";
+// import styles from "../styles/Home.module.css";
+import { Button } from "@tundra/shared";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Hello World</Button>
+    </div>
+  );
+}
